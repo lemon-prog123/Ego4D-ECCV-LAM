b988c179dae532f690768646420e87fe9c08affb
Lemon-prog123 <2000013085@stu.pku.edu.cn>
2022-11-16 23:04:46 +0800

    Ego4D@lam

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
-nohup python -u run_copy.py  --head_query  --batch_size 32   --lr 1e-4 --val_stride 13  --o --filter --train_stride 3 --model GazeDETR --num_workers 3 --train_path output_gazequeryvaltrain --exp_path output_gazequeryval >gazequeryval.out &
+nohup python -u run.py  --head_query  --batch_size 32   --lr 1e-4 --val_stride 13  --o --filter --train_stride 3 --model GazeDETR --num_workers 3 --train_path output_gazequeryvaltrain --exp_path output_gazequeryval >gazequeryval.out &
